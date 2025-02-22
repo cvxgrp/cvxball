770978da51bbe235c9030467a3497e4365e59138
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-22 10:03:58 +0400

    server back on Ball problem

--- src/example/client.py
+++ src/example/client.py
@@ -1,18 +1,20 @@
+import numpy as np
 from np.flight import Client
 
 
 def main():
     # Connect to the server
-    with Client("grpc+tcp://127.0.0.1:8080"):  # as client:
+    with Client("grpc+tls://cvxball-710171668953.us-central1.run.app:443") as client:
         # Example data
         # The server is expecting a dictionary of numpy arrays
-        # data = {"input": np.random.randn(2000, 10)}
+        data = {"input": np.random.randn(2000, 10)}
 
         # The server will return a dictionary of numpy arrays
-        # results = client.compute(command="test", data=data)
+        results = client.compute(command="test", data=data)
 
         # logger.info(f"Results: {results.keys()}")
         print("Hello World!")
+        print(results)
         pass
 
 
